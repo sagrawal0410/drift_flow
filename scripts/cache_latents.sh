679d9ec2738c6e312a7c5204fe9a6cdc177cb3d6
sagrawal0410 <agrawalshaurya1006@gmail.com>
2026-02-11 13:30:40 -0500

    1 gpu

--- scripts/cache_latents.sh
+++ scripts/cache_latents.sh
@@ -5,7 +5,7 @@
 #SBATCH --qos=vision-he-low
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-node=8
+#SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=64G
 #SBATCH --time=04:00:00
@@ -21,15 +21,17 @@ mkdir -p logs
 conda activate drift_flow
 
 # Cache train split
-torchrun --nproc_per_node=4 dataset/cache_latent.py \
+python dataset/cache_latent.py \
     --data_path /data/infolab/aia/imagenet_pytorch/train \
     --cached_path /data/scratch-oc40/shaurya10/cache_latents/train \
     --split train \
     --batch_size 128
 
+
 # Cache val split
-torchrun --nproc_per_node=4 dataset/cache_latent.py \
+python dataset/cache_latent.py \
     --data_path /data/infolab/aia/imagenet_pytorch/val \
     --cached_path /data/scratch-oc40/shaurya10/cache_latents/val \
     --split val \
     --batch_size 128
+
