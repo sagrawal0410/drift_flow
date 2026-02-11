b4253f9de2269cb5c97086d080f5a97934f34124
sagrawal0410 <agrawalshaurya1006@gmail.com>
2026-02-11 01:39:14 -0500

    Multi-node for tokenization of images and caching

--- scripts/cache_latents.sh
+++ scripts/cache_latents.sh
@@ -5,7 +5,7 @@
 #SBATCH --qos=vision-he-low
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-node=1
+#SBATCH --gpus-per-node=8
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=64G
 #SBATCH --time=04:00:00
@@ -18,18 +18,18 @@ CACHE_PATH=/path/to/cache
 
 mkdir -p logs
 
-conda activate drift
+conda activate drift_flow
 
 # Cache train split
-python dataset/cache_latent.py \
-    --data_path "$DATA_PATH" \
-    --cached_path "${CACHE_PATH}/train" \
+torchrun --nproc_per_node=4 dataset/cache_latent.py \
+    --data_path /data/infolab/aia/imagenet_pytorch/train \
+    --cached_path /data/scratch-oc40/shaurya10/cache_latents/train \
     --split train \
     --batch_size 128
 
 # Cache val split
-python dataset/cache_latent.py \
-    --data_path "$DATA_PATH" \
-    --cached_path "${CACHE_PATH}/val" \
-    --split val \
+torchrun --nproc_per_node=4 dataset/cache_latent.py \
+    --data_path /data/infolab/aia/imagenet_pytorch/val \
+    --cached_path /data/scratch-oc40/shaurya10/cache_latents/train \
+    --split train \
     --batch_size 128
