d55adb41a3f6f1485b697335114c3aace2d3b090
sagrawal0410 <agrawalshaurya1006@gmail.com>
2026-02-11 14:43:31 -0500

    Dataset to FID File created

---
+++ utils/global_path.py
@@ -0,0 +1,6 @@
+ckpt_root = "/data/scratch-oc40/shaurya10/drift_flow/checkpoints/"
+dataset_to_fid_path = {
+    "imagenet256": "/data/scratch-oc40/shaurya10/drift_flow/utils/fid_stats/adm_in256_stats.npz",
+}
+prc_recall_path = ""
+imagenet_root = "/data/infolab/aia/imagenet_pytorch"
